2978bb681d3a176a5c8dbd63e54031bcd0990018
bxr5xrk <vasyldobrovoslkyi@gmail.com>
2024-08-01 13:14:33 +0300

    feat[pmk]: added assets

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,5 +1,4 @@
 import { resolve } from "node:path";
 
-export const REQUEST_FILE_PATH = resolve(__dirname, "..", 'request.txt');
-
+export const REQUEST_FILE_PATH = resolve(__dirname, "..", "assets", 'request.txt');
 export const COLLECT_ATTEMPTS = 7;
